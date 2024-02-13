b56ae55368c481ba9c4b75150aedd34c70da74a8
eloimaduell <eloi@playmodes.com>
2024-02-13 11:31:55 +0100

    changed BPMControl. Added PhaseReset void, phase reset on bpm change.

--- src/BPMControl.h
+++ src/BPMControl.h
@@ -7,11 +7,24 @@
 class BPMControl : public ofxOceanodeNodeModel {
 public:
     BPMControl() : ofxOceanodeNodeModel("BPM Control") {
+        lastBPM=0;
+        
+        addParameter(phaseReset.set("PhaseRst"));
         addParameter(bpm.set("BPM", 120.0f, 1.0f, 999.9f)); // Assuming a range from 1 to 999 BPM
-        addParameter(port.set("Port", 11575, 1, 65535)); // Range for valid port numbers
-
+        addParameter(port.set("Port", 12345, 1, 65535)); // Range for valid port numbers
+        addParameter(resetOnChg.set("RstPhOnChg",true));
+        
+        phaseResetListener = phaseReset.newListener([this](){
+            sendPhaseReset();
+        });
+        
         bpmListener = bpm.newListener([this](float &newBpm){
             sendBPM(newBpm);
+            if(lastBPM!=bpm)
+            {
+                if(resetOnChg) sendPhaseReset();
+                lastBPM=bpm;
+            }
         });
 
         portListener = port.newListener([this](int &newPort){
@@ -32,13 +45,23 @@ private:
         m.addFloatArg(newBpm);
         sender.sendMessage(m, false);
     }
+    void sendPhaseReset(){
+        ofxOscMessage m;
+        m.setAddress("/phaseReset");
+        sender.sendMessage(m, false);
+    }
 
     ofParameter<float> bpm;
     ofParameter<int> port;
+    ofParameter<void> phaseReset;
+    ofParameter<bool> resetOnChg;
     ofxOscSender sender;
 
     ofEventListener bpmListener;
     ofEventListener portListener;
+    ofEventListener phaseResetListener;
+    
+    float lastBPM;
 };
 
 #endif /* BPMControl_h */
