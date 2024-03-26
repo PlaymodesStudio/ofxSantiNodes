2de37b047d1d978ff06db7a4b80e0abeaae2c92b
Santi_Vilanova <santi@playmodes.com>
2024-03-26 16:07:05 +0100

    new node: merge void, which merges multiple void signals into a single output

---
+++ src/mergeVoid.cpp
@@ -0,0 +1,1 @@
+#include "mergeVoid.h"
---
+++ src/mergeVoid.h
@@ -0,0 +1,34 @@
+#ifndef mergeVoid_h
+#define mergeVoid_h
+
+#include "ofxOceanodeNodeModel.h"
+
+class mergeVoid : public ofxOceanodeNodeModel {
+public:
+    mergeVoid() : ofxOceanodeNodeModel("Merge Void") {
+        description = "Merges void signals from various inputs into one output";
+
+        // Output void parameter
+        addParameter(voidOut.set("Void Out"));
+
+        // Setup void input parameters and listen to them
+        for (int i = 0; i < 4; ++i) {
+            ofParameter<void> newInput;
+            newInput.setName("In " + ofToString(i + 1));
+            addParameter(newInput);
+            inputs.push_back(newInput);
+
+            // Directly attach listeners to input parameters
+            listeners[i] = newInput.newListener([this]() {
+                this->voidOut.trigger(); // Trigger the output when any input is activated
+            });
+        }
+    }
+
+private:
+    ofParameter<void> voidOut; // Void output parameter
+    std::vector<ofParameter<void>> inputs; // Store input parameters
+    std::array<ofEventListener, 4> listeners; // Use an array for listeners
+};
+
+#endif /* mergeVoid_h */
