9d92e1ff4c73a4349a266888d25bc3b794d81731
Santi_Vilanova <santi@playmodes.com>
2024-07-16 15:25:29 +0200

    added modulo

--- src/cycleCount.h
+++ src/cycleCount.h
@@ -12,12 +12,13 @@ public:
         addParameter(input.set("Input", {0}, {0}, {1}));
         addParameter(resetCount.set("Reset"));
         addParameter(resetNext.set("Reset Next"));
+        addParameter(mod.set("Mod", 64, 1, 1000)); 
         addOutputParameter(countOutput.set("Count", {0}, {0}, {INT_MAX}));
         addOutputParameter(resetOut.set("Reset Out"));
 
         listeners.push(resetCount.newListener([this](){
             std::fill(fallingEdgeCount.begin(), fallingEdgeCount.end(), 0);
-            countOutput = fallingEdgeCount;
+            updateCountOutput();
         }));
 
         listeners.push(resetNext.newListener([this](){
@@ -44,7 +45,7 @@ public:
             }
             lastInput = vf;
 
-            countOutput = fallingEdgeCount;
+            updateCountOutput();
         }));
     }
 
@@ -52,6 +53,7 @@ private:
     ofParameter<vector<float>> input;
     ofParameter<void> resetCount;
     ofParameter<void> resetNext;
+    ofParameter<int> mod; // Modulo parameter
     ofParameter<vector<int>> countOutput;
     ofParameter<void> resetOut; // 'Reset Out' void output parameter
 
@@ -60,6 +62,18 @@ private:
     vector<bool> shouldResetNextCycle;
 
     ofEventListeners listeners;
+
+    void updateCountOutput() {
+        vector<int> modulatedCountOutput(fallingEdgeCount.size());
+        int modValue = mod.get();
+
+        for (size_t i = 0; i < fallingEdgeCount.size(); ++i) {
+            modulatedCountOutput[i] = fallingEdgeCount[i] % modValue;
+        }
+
+        countOutput = modulatedCountOutput;
+    }
 };
 
 #endif /* cycleCount_h */
+
