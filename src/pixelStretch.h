fc664a1cc5ee4c110de1cc09970ab767a29b5df1
Santi_Vilanova <santi@playmodes.com>
2024-08-24 22:20:39 +0200

    new pixelstretch

--- src/pixelStretch.h
+++ src/pixelStretch.h
@@ -1,5 +1,5 @@
-#ifndef PixelStretch_h
-#define PixelStretch_h
+#ifndef pixelStretch_h
+#define pixelStretch_h
 
 #include "ofxOceanodeNodeModel.h"
 
@@ -7,79 +7,53 @@ class pixelStretch : public ofxOceanodeNodeModel {
 public:
     pixelStretch() : ofxOceanodeNodeModel("Pixel Stretch") {}
 
-    void setup() override {
-        description = "Stretches the column of pixels at a given x position to all columns to the left of x.";
-
+    void setup() {
         addParameter(input.set("Input", nullptr));
-        addParameter(x.set("X", 0, 0, ofGetWidth()));
+        addParameter(x.set("X", 0, 0, 2600));
         addOutputParameter(output.set("Output", nullptr));
 
-        input.addListener(this, &pixelStretch::onInputChanged);
-        fboAllocated = false;
+        color = ofColor::orangeRed;
+
+        loadShader();
     }
 
-    void update(ofEventArgs &a) {
-        if (inputChanged) {
-            updateTexture();
-            inputChanged = false;
+    void update(ofEventArgs &e) {
+        if(input.get() == nullptr) return;
+
+        if(!fbo.isAllocated() || fbo.getWidth() != input.get()->getWidth() || fbo.getHeight() != input.get()->getHeight()) {
+            fbo.allocate(input.get()->getWidth(), input.get()->getHeight(), GL_RGBA);
         }
-    }
 
-    void activate() override {
-        ofAddListener(ofEvents().update, this, &pixelStretch::update);
-    }
+        fbo.begin();
+        shader.begin();
+        shader.setUniformTexture("tSource", *input.get(), 0);
+        shader.setUniform1f("x", x);
+        input.get()->draw(0, 0);
+        shader.end();
+        fbo.end();
 
-    void deactivate() override {
-        ofRemoveListener(ofEvents().update, this, &pixelStretch::update);
-        fbo.clear();
-        output = nullptr;
-    }
-
-    void onInputChanged(ofTexture* &newInput) {
-        inputChanged = true;
-    }
-
-    void updateTexture() {
-        if (input.get() != nullptr) {
-            ofPixels pixels;
-            input.get()->readToPixels(pixels);
-
-            if (!fboAllocated || fbo.getWidth() != pixels.getWidth() || fbo.getHeight() != pixels.getHeight()) {
-                fbo.allocate(pixels.getWidth(), pixels.getHeight(), GL_RGBA32F);
-                fbo.getTexture().setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
-                fboAllocated = true;
-            }
-
-            int height = pixels.getHeight();
-            std::vector<ofColor> columnPixels(height);
-            for (int row = 0; row < height; row++) {
-                columnPixels[row] = pixels.getColor(x, row);
-            }
-
-            for (int col = x - 1; col >= 0; col--) {
-                for (int row = 0; row < height; row++) {
-                    pixels.setColor(col, row, columnPixels[row]);
-                }
-            }
-
-            fbo.begin();
-            ofClear(0, 0, 0, 255);
-            ofTexture tempTexture;
-            tempTexture.loadData(pixels);
-            tempTexture.draw(0, 0);
-            fbo.end();
-
-            output = &fbo.getTexture();
-        }
+        output = &fbo.getTexture();
     }
 
 private:
     ofParameter<ofTexture*> input;
-    ofParameter<int> x;
+    ofParameter<float> x;
     ofParameter<ofTexture*> output;
     ofFbo fbo;
-    bool fboAllocated;
-    bool inputChanged = false;
+    ofShader shader;
+
+    void loadShader() {
+        string shaderPath = "Effects/PixelStretch.glsl";
+        ofBuffer buffer = ofBufferFromFile(shaderPath);
+        
+        if(buffer.size()) {
+            string shaderSource = buffer.getText();
+            shader.setupShaderFromSource(GL_FRAGMENT_SHADER, shaderSource);
+            shader.linkProgram();
+        } else {
+            ofLogError("pixelStretch") << "Unable to load shader file: " << shaderPath;
+        }
+    }
 };
 
-#endif /* PixelStretch_h */
+#endif /* pixelStretch_h */
