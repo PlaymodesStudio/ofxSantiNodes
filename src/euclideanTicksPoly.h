0a0f0dd7dc8e791431563a95fddb4a59f8928fb6
Santi_Vilanova <santi@playmodes.com>
2024-11-17 02:53:57 +0100

    reverted to working version

--- src/euclideanTicksPoly.h
+++ src/euclideanTicksPoly.h
@@ -8,41 +8,32 @@
 class euclideanTicksPoly : public ofxOceanodeNodeModel {
 public:
     euclideanTicksPoly() : ofxOceanodeNodeModel("Euclidean Ticks Poly") {}
-    
+
     void setup();
-    void update(ofEventArgs &args);  // Add the update method
     void calculate();
     void reset();
-    
+
 private:
-    // Input Parameters
     ofParameter<std::vector<int>> inCount;
     ofParameter<std::vector<int>> length;
     ofParameter<std::vector<int>> onsets;
     ofParameter<std::vector<int>> offset;
-    ofParameter<bool> retrigger;
-    
-    // Output Parameters
     ofParameter<std::vector<float>> gatesOut;
+    ofParameter<std::vector<float>> gates2Out;  // New gates2Out parameter
+    std::vector<float> prevGatesOut;            // To track the previous state of gatesOut
+    
     ofParameter<std::vector<int>> outCount;
-    ofParameter<void> tick;
-    
-    // Control Parameters
     ofParameter<void> resetButton;
-    ofParameter<void> resetNext;
-    
-    // Event Listeners
+    ofParameter<void> resetNext;  // New ResetNext parameter
+    ofParameter<void> tick;
+
     ofEventListener listener;
     ofEventListener resetButtonListener;
-    ofEventListener resetNextListener;
-    ofEventListener updateListener;  // Listener for the update method
-    
-    // Internal State Variables
+    ofEventListener resetNextListener;  // Listener for the resetNext
+
     std::vector<int> outCountVector;
     std::vector<int> prevInCount;
-    std::vector<float> previousGatesOut;
-    std::vector<bool> insertZeroNextFrame;  // Flag for inserting zero in the next frame
-    bool shouldResetNext = false;
+    bool shouldResetNext = false;  // To track if a reset is needed on the next matching number
 };
 
 #endif /* euclideanTicksPoly_h */
