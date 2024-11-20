14602fa08d81d4d2a2dab2710a401213c7b645b7
Santi_Vilanova <santi@playmodes.com>
2024-11-20 09:29:17 +0100

    deleted radio folder. now everything lives in a new radio addon

--- src/radio/run_python.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PYTHONPATH="/Users/santiagovilanova/Documents/_OF/openFrameworks_GIT_PM/apps/Liceu/OceanodeScroller/bin/data/radio/venv/lib/python3.9/site-packages:$PYTHONPATH"
-export DYLD_LIBRARY_PATH="/Applications/VLC.app/Contents/MacOS/lib:$DYLD_LIBRARY_PATH"
-export VLC_PLUGIN_PATH="/Applications/VLC.app/Contents/MacOS/plugins"
-source "/Users/santiagovilanova/Documents/_OF/openFrameworks_GIT_PM/apps/Liceu/OceanodeScroller/bin/data/radio/venv/bin/activate"
-"/Users/santiagovilanova/Documents/_OF/openFrameworks_GIT_PM/apps/Liceu/OceanodeScroller/bin/data/radio/venv/bin/python3" "$@"
