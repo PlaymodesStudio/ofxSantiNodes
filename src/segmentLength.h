a9231920876ddbf729e2ca7f815bc00e3947a4b2
eloimaduell <eloi@playmodes.com>
2025-02-11 18:14:19 +0100

    [SegmentLength] : added check that input vectors (pointsX and pointsY) have size != 0 and pointsX and pointsY size is the same.

--- src/segmentLength.h
+++ src/segmentLength.h
@@ -23,45 +23,49 @@ public:
     }
     
     void calculate() {
-        vector<float> aux;
-        vector<float> midX;
-        vector<float> midY;
-        glm::vec2 point1, point2;
-        
-        int i = 0;
-        while(i < pointsX.get().size()) {
-            // Check for the separator
-            if(separator && (pointsX.get()[i] == -1 || pointsY.get()[i] == -1)) {
-                i++;
-                continue;
-            }
+		if(pointsX.get().size()!=0 && pointsY.get().size()!=0 && pointsX.get().size()==pointsY.get().size())
+		{
+			vector<float> aux;
+			vector<float> midX;
+			vector<float> midY;
+			glm::vec2 point1, point2;
+			
+			int i = 0;
+			while(i < pointsX.get().size()) {
+				// Check for the separator
+				if(separator && (pointsX.get()[i] == -1 || pointsY.get()[i] == -1)) {
+					i++;
+					continue;
+				}
 
-            // Get first point
-            point1.x = pointsX.get()[i];
-            point1.y = pointsY.get()[i];
-            i++;
-            
-            // If there are no more points or if a separator is found, continue to the next loop
-            if(i >= pointsX.get().size() || (separator && (pointsX.get()[i] == -1 || pointsY.get()[i] == -1))) {
-                continue;
-            }
-            
-            // Get second point
-            point2.x = pointsX.get()[i];
-            point2.y = pointsY.get()[i];
-            
-            // Calculate distance and add it to the aux vector
-            aux.push_back(glm::distance(point1, point2));
-            
-            // Calculate and store midpoints
-            midX.push_back((point1.x + point2.x) / 2.0f);
-            midY.push_back((point1.y + point2.y) / 2.0f);
-        }
-        
-        lengths = aux;
-        output = lengths;  // Copy the lengths to the output parameter for visualization
-        midpointX = midX;  // Set midpoint X output
-        midpointY = midY;  // Set midpoint Y output
+				// Get first point
+				point1.x = pointsX.get()[i];
+				point1.y = pointsY.get()[i];
+				i++;
+				
+				// If there are no more points or if a separator is found, continue to the next loop
+				if(i >= pointsX.get().size() || (separator && (pointsX.get()[i] == -1 || pointsY.get()[i] == -1))) {
+					continue;
+				}
+				
+				// Get second point
+				point2.x = pointsX.get()[i];
+				point2.y = pointsY.get()[i];
+				
+				// Calculate distance and add it to the aux vector
+				aux.push_back(glm::distance(point1, point2));
+				
+				// Calculate and store midpoints
+				midX.push_back((point1.x + point2.x) / 2.0f);
+				midY.push_back((point1.y + point2.y) / 2.0f);
+			}
+			
+			lengths = aux;
+			output = lengths;  // Copy the lengths to the output parameter for visualization
+			midpointX = midX;  // Set midpoint X output
+			midpointY = midY;  // Set midpoint Y output
+
+		}
     }
     
     void update(ofEventArgs &a) override {
