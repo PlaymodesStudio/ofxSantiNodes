017d981ed45f313dc2f5906a3215088d71221817
eloimaduell <eloi@playmodes.com>
2025-02-11 18:12:14 +0100

    [Logic] : Added control to check that both input vectors don't have size 9.

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -22,35 +22,38 @@ void Logic::setup() {
 }
 
 void Logic::computeLogic() {
-    auto getValueForIndex = [](const vector<float> &vf, int i) -> float {
-        if(i < vf.size()){
-            return vf[i];
-        }else{
-            return vf[0];
-        }
-    };
+	if(input1.get().size()!=0 && input2.get().size()!=0)
+	{
+		auto getValueForIndex = [](const vector<float> &vf, int i) -> float {
+			if(i < vf.size()){
+				return vf[i];
+			}else{
+				return vf[0];
+			}
+		};
 
-    vector<float> result;
-    vector<string> operations = {">", ">=", "<", "<=", "==", "!=", "&&", "||", "!>", "!<", "!>=", "!<="};
+		vector<float> result;
+		vector<string> operations = {">", ">=", "<", "<=", "==", "!=", "&&", "||", "!>", "!<", "!>=", "!<="};
 
-    for (size_t i = 0; i < std::max(input1->size(), input2->size()); ++i) {
-        bool logicResult = false;
-        string op = operations[operation.get()];  // Get operation string from operations vector
-        float val1 = getValueForIndex(input1, i);
-        float val2 = getValueForIndex(input2, i);
-        if (op == ">") logicResult = val1 > val2;
-        else if (op == ">=") logicResult = val1 >= val2;
-        else if (op == "<") logicResult = val1 < val2;
-        else if (op == "<=") logicResult = val1 <= val2;
-        else if (op == "==") logicResult = val1 == val2;
-        else if (op == "!=") logicResult = val1 != val2;
-        else if (op == "&&") logicResult = static_cast<bool>(val1) && static_cast<bool>(val2);
-        else if (op == "||") logicResult = static_cast<bool>(val1) || static_cast<bool>(val2);
-        else if (op == "!>") logicResult = !(val1 > val2);
-        else if (op == "!<") logicResult = !(val1 < val2);
-        else if (op == "!>=") logicResult = !(val1 >= val2);
-        else if (op == "!<=") logicResult = !(val1 <= val2);
-        result.push_back(logicResult ? 1.0f : 0.0f);
-    }
-    output = result;
+		for (size_t i = 0; i < std::max(input1->size(), input2->size()); ++i) {
+			bool logicResult = false;
+			string op = operations[operation.get()];  // Get operation string from operations vector
+			float val1 = getValueForIndex(input1, i);
+			float val2 = getValueForIndex(input2, i);
+			if (op == ">") logicResult = val1 > val2;
+			else if (op == ">=") logicResult = val1 >= val2;
+			else if (op == "<") logicResult = val1 < val2;
+			else if (op == "<=") logicResult = val1 <= val2;
+			else if (op == "==") logicResult = val1 == val2;
+			else if (op == "!=") logicResult = val1 != val2;
+			else if (op == "&&") logicResult = static_cast<bool>(val1) && static_cast<bool>(val2);
+			else if (op == "||") logicResult = static_cast<bool>(val1) || static_cast<bool>(val2);
+			else if (op == "!>") logicResult = !(val1 > val2);
+			else if (op == "!<") logicResult = !(val1 < val2);
+			else if (op == "!>=") logicResult = !(val1 >= val2);
+			else if (op == "!<=") logicResult = !(val1 <= val2);
+			result.push_back(logicResult ? 1.0f : 0.0f);
+		}
+		output = result;
+	}
 }
