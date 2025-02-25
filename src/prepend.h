af900b2e5a219ebc6fbef4ff41f4495bcbe5408e
santiago vilanova <santiagovilanova@MacBook-Pro-de-santiago-2.local>
2025-02-25 20:05:57 +0100

    [prepend]: several bugfixes

--- src/prepend.h
+++ src/prepend.h
@@ -5,48 +5,59 @@
 
 class prepend : public ofxOceanodeNodeModel {
 public:
-    prepend() : ofxOceanodeNodeModel("Prepend") {}
-    
-    void setup() override {
-        description = "Prepends a specified string to the input string, adding a space between them";
-        
-        addParameter(inputString.set("Input", ""));
-        addParameter(prependString.set("Prepend", ""));
-        addOutputParameter(outputString.set("Output", ""));
-        
-        listener = inputString.newListener([this](string &){
-            process();
-        });
-        
-        prependListener = prependString.newListener([this](string &){
-            process();
-        });
-    }
-    
+	prepend() : ofxOceanodeNodeModel("Prepend") {}
+	
+	void setup() override {
+		description = "Prepends a specified string to the input string. Optional space can be added between them";
+		
+		addParameter(inputString.set("Input", ""));
+		addParameter(prependString.set("Prepend", ""));
+		addParameter(addSpace.set("Add Space", false));
+		addOutputParameter(outputString.set("Output", ""));
+		
+		listener = inputString.newListener([this](string &){
+			process();
+		});
+		
+		prependListener = prependString.newListener([this](string &){
+			process();
+		});
+
+		spaceListener = addSpace.newListener([this](bool &){
+			process();
+		});
+	}
+	
 private:
-    void process() {
-        string prefix = prependString.get();
-        string input = inputString.get();
-        
-        // Handle empty strings
-        if(prefix.empty()) {
-            outputString = input;
-            return;
-        }
-        if(input.empty()) {
-            outputString = prefix;
-            return;
-        }
-        
-        // Combine strings with a space in between
-        outputString = prefix + " " + input;
-    }
-    
-    ofParameter<string> inputString;
-    ofParameter<string> prependString;
-    ofParameter<string> outputString;
-    ofEventListener listener;
-    ofEventListener prependListener;
+	void process() {
+		string prefix = prependString.get();
+		string input = inputString.get();
+		
+		// Handle empty strings
+		if(prefix.empty()) {
+			outputString = input;
+			return;
+		}
+		if(input.empty()) {
+			outputString = prefix;
+			return;
+		}
+		
+		// Combine strings with optional space
+		if(addSpace) {
+			outputString = prefix + " " + input;
+		} else {
+			outputString = prefix + input;
+		}
+	}
+	
+	ofParameter<string> inputString;
+	ofParameter<string> prependString;
+	ofParameter<string> outputString;
+	ofParameter<bool> addSpace;
+	ofEventListener listener;
+	ofEventListener prependListener;
+	ofEventListener spaceListener;
 };
 
 #endif /* prepend_h */
