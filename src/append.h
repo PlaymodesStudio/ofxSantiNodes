999b5caf4969507e30a3e1554a4c2006e65fe09e
santiago vilanova <santiagovilanova@MacBook-Pro-de-santiago-2.local>
2025-02-25 20:05:44 +0100

    [append]: several bugfixes

--- src/append.h
+++ src/append.h
@@ -5,48 +5,59 @@
 
 class append : public ofxOceanodeNodeModel {
 public:
-    append() : ofxOceanodeNodeModel("Append") {}
-    
-    void setup() override {
-        description = "Appends a specified string to the input string, adding a space between them";
-        
-        addParameter(inputString.set("Input", ""));
-        addParameter(appendString.set("Append", ""));
-        addOutputParameter(outputString.set("Output", ""));
-        
-        listener = inputString.newListener([this](string &){
-            process();
-        });
-        
-        appendListener = appendString.newListener([this](string &){
-            process();
-        });
-    }
-    
+	append() : ofxOceanodeNodeModel("Append") {}
+	
+	void setup() override {
+		description = "Appends a specified string to the input string. Optional space can be added between them";
+		
+		addParameter(inputString.set("Input", ""));
+		addParameter(appendString.set("Append", ""));
+		addParameter(addSpace.set("Add Space", false));
+		addOutputParameter(outputString.set("Output", ""));
+		
+		listener = inputString.newListener([this](string &){
+			process();
+		});
+		
+		appendListener = appendString.newListener([this](string &){
+			process();
+		});
+
+		spaceListener = addSpace.newListener([this](bool &){
+			process();
+		});
+	}
+	
 private:
-    void process() {
-        string input = inputString.get();
-        string suffix = appendString.get();
-        
-        // Handle empty strings
-        if(suffix.empty()) {
-            outputString = input;
-            return;
-        }
-        if(input.empty()) {
-            outputString = suffix;
-            return;
-        }
-        
-        // Combine strings with a space in between
-        outputString = input + " " + suffix;
-    }
-    
-    ofParameter<string> inputString;
-    ofParameter<string> appendString;
-    ofParameter<string> outputString;
-    ofEventListener listener;
-    ofEventListener appendListener;
+	void process() {
+		string input = inputString.get();
+		string suffix = appendString.get();
+		
+		// Handle empty strings
+		if(suffix.empty()) {
+			outputString = input;
+			return;
+		}
+		if(input.empty()) {
+			outputString = suffix;
+			return;
+		}
+		
+		// Combine strings with optional space
+		if(addSpace) {
+			outputString = input + " " + suffix;
+		} else {
+			outputString = input + suffix;
+		}
+	}
+	
+	ofParameter<string> inputString;
+	ofParameter<string> appendString;
+	ofParameter<string> outputString;
+	ofParameter<bool> addSpace;
+	ofEventListener listener;
+	ofEventListener appendListener;
+	ofEventListener spaceListener;
 };
 
 #endif /* append_h */
