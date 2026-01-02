4b71c4d015c78df355c1339a60b886661425672f
santiago vilanova <santiagovilanova@MacBook-Pro-de-santiago-2.local>
2026-01-02 19:52:52 +0100

    [Logic]: added  XOR, NAND, NOR, XNOR, NOT1 and NOT2

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -7,18 +7,19 @@
 Logic::Logic() : ofxOceanodeNodeModel("Logic") {}
 
 void Logic::setup() {
-    addParameter(input1.set("Input 1", {0}, {-FLT_MAX}, {FLT_MAX}));
-    addParameter(input2.set("Input 2", {0}, {-FLT_MAX}, {FLT_MAX}));
-    addParameterDropdown(operation, "Op", 0,
-      {">", ">=", "<", "<=", "==", "!=", "&&", "||", "!>", "!<", "!>=", "!<="});
-    addOutputParameter(output.set("Output", {0}, {0}, {1}));
+	addParameter(input1.set("Input 1", {0}, {-FLT_MAX}, {FLT_MAX}));
+	addParameter(input2.set("Input 2", {0}, {-FLT_MAX}, {FLT_MAX}));
+	addParameterDropdown(operation, "Op", 0,
+	  {">", ">=", "<", "<=", "==", "!=", "&&", "||", "!>", "!<", "!>=", "!<=",
+	   "XOR", "NAND", "NOR", "XNOR", "NOT1", "NOT2"});
+	addOutputParameter(output.set("Output", {0}, {0}, {1}));
 
-    listeners.push(input1.newListener([this](vector<float>& vf) {
-        computeLogic();
-    }));
-    listeners.push(input2.newListener([this](vector<float>& vf) {
-        computeLogic();
-    }));
+	listeners.push(input1.newListener([this](vector<float>& vf) {
+		computeLogic();
+	}));
+	listeners.push(input2.newListener([this](vector<float>& vf) {
+		computeLogic();
+	}));
 }
 
 void Logic::computeLogic() {
@@ -33,25 +34,37 @@ void Logic::computeLogic() {
 		};
 
 		vector<float> result;
-		vector<string> operations = {">", ">=", "<", "<=", "==", "!=", "&&", "||", "!>", "!<", "!>=", "!<="};
+		vector<string> operations = {">", ">=", "<", "<=", "==", "!=", "&&", "||",
+									 "!>", "!<", "!>=", "!<=",
+									 "XOR", "NAND", "NOR", "XNOR", "NOT1", "NOT2"};
 
 		for (size_t i = 0; i < std::max(input1->size(), input2->size()); ++i) {
 			bool logicResult = false;
-			string op = operations[operation.get()];  // Get operation string from operations vector
+			string op = operations[operation.get()];
 			float val1 = getValueForIndex(input1, i);
 			float val2 = getValueForIndex(input2, i);
+			bool bool1 = static_cast<bool>(val1);
+			bool bool2 = static_cast<bool>(val2);
+			
 			if (op == ">") logicResult = val1 > val2;
 			else if (op == ">=") logicResult = val1 >= val2;
 			else if (op == "<") logicResult = val1 < val2;
 			else if (op == "<=") logicResult = val1 <= val2;
 			else if (op == "==") logicResult = val1 == val2;
 			else if (op == "!=") logicResult = val1 != val2;
-			else if (op == "&&") logicResult = static_cast<bool>(val1) && static_cast<bool>(val2);
-			else if (op == "||") logicResult = static_cast<bool>(val1) || static_cast<bool>(val2);
+			else if (op == "&&") logicResult = bool1 && bool2;
+			else if (op == "||") logicResult = bool1 || bool2;
 			else if (op == "!>") logicResult = !(val1 > val2);
 			else if (op == "!<") logicResult = !(val1 < val2);
 			else if (op == "!>=") logicResult = !(val1 >= val2);
 			else if (op == "!<=") logicResult = !(val1 <= val2);
+			else if (op == "XOR") logicResult = bool1 != bool2;  // XOR: true if different
+			else if (op == "NAND") logicResult = !(bool1 && bool2);  // NAND: not AND
+			else if (op == "NOR") logicResult = !(bool1 || bool2);  // NOR: not OR
+			else if (op == "XNOR") logicResult = bool1 == bool2;  // XNOR: true if same
+			else if (op == "NOT1") logicResult = !bool1;  // NOT of input 1
+			else if (op == "NOT2") logicResult = !bool2;  // NOT of input 2
+			
 			result.push_back(logicResult ? 1.0f : 0.0f);
 		}
 		output = result;
