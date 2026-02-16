bb924ee4b81ca5da9da898d9b3b9bcd99550bb4b
Santi Vilanova <santi@playmodes.com>
2026-02-17 00:26:42 +0100

    [indexRouter]: added VecSize

--- src/indexRouter.h
+++ src/indexRouter.h
@@ -10,9 +10,10 @@ public:
 
 	void setup() override {
 		description = "Routes input values to specified output indices. Output size is determined by the maximum index value + 1. Missing indices are filled with 0 or -1.";
-		
+
 		addParameter(input.set("Input", {0}, {-FLT_MAX}, {FLT_MAX}));
 		addParameter(indices.set("Indices", {0}, {0}, {INT_MAX}));
+		addParameter(vectorSize.set("VecSize", -1, -1, INT_MAX));
 		addParameter(useMinusOne.set("-1", false));
 		addOutputParameter(output.set("Output", {0}, {-FLT_MAX}, {FLT_MAX}));
 
@@ -21,6 +22,10 @@ public:
 		}));
 
 		listeners.push(indices.newListener([this](vector<int> &vi){
+			processRouting();
+		}));
+
+		listeners.push(vectorSize.newListener([this](int &i){
 			processRouting();
 		}));
 
@@ -33,20 +38,27 @@ private:
 	void processRouting() {
 		const auto& inputVec = input.get();
 		const auto& indicesVec = indices.get();
-		
+
 		// If either input is empty, return empty output
 		if (inputVec.empty() || indicesVec.empty()) {
 			output = vector<float>();
 			return;
 		}
-		
-		// Find the maximum index value to determine output size
-		int maxIndex = *std::max_element(indicesVec.begin(), indicesVec.end());
-		
-		// Create output vector with size maxIndex + 1
-		int outputSize = maxIndex + 1;
+
+		// Determine output size
+		int outputSize;
+		if (vectorSize == -1) {
+			// Dynamic sizing: use maximum index value + 1
+			int maxIndex = *std::max_element(indicesVec.begin(), indicesVec.end());
+			outputSize = maxIndex + 1;
+		} else {
+			// Fixed sizing: use specified vector size
+			outputSize = vectorSize;
+		}
+
+		// Create output vector filled with default value
 		vector<float> result(outputSize, useMinusOne ? -1.0f : 0.0f);
-		
+
 		// Map input values to their corresponding output indices
 		size_t mapCount = std::min(inputVec.size(), indicesVec.size());
 		for (size_t i = 0; i < mapCount; ++i) {
@@ -55,12 +67,13 @@ private:
 				result[targetIndex] = inputVec[i];
 			}
 		}
-		
+
 		output = result;
 	}
 
 	ofParameter<vector<float>> input;
 	ofParameter<vector<int>> indices;
+	ofParameter<int> vectorSize;
 	ofParameter<bool> useMinusOne;
 	ofParameter<vector<float>> output;
 
