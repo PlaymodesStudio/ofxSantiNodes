525f8f80f1bf16ec0a930dadaa4b9ba443307fff
Santi Vilanova <santi@playmodes.com>
2026-03-02 14:04:50 +0100

    [distribute]: added dynamic output number

--- src/distribute.h
+++ src/distribute.h
@@ -5,86 +5,113 @@
 
 class distribute : public ofxOceanodeNodeModel {
 public:
-    distribute() : ofxOceanodeNodeModel("Distribute"), routeUpdateNeeded(true) {
-        description = "Routes each element of the incoming data to one of two outputs based on the 'Route To' setting per element. Optionally keeps the last value on the unoccupied output. The 'Event' toggle enables processing only on new unique inputs.";
+    distribute() : ofxOceanodeNodeModel("Distribute") {
+        description = "Routes each element of the incoming data to one of N outputs based on the 'Route To' setting per element. Optionally keeps the last value on the unoccupied outputs. The 'Event' toggle enables processing only on new unique inputs.";
 
+        addInspectorParameter(numOutputs.set("Num Outputs", 2, 1, 16));
+    }
+
+    void setup() override {
         addParameter(input.set("Input", {0}, {-FLT_MAX}, {FLT_MAX}));
-        addParameter(routeTo.set("Route To", {1}, {1}, {2})); // Now a vector of integers
+        addParameter(routeTo.set("Route To", {1}, {1}, {numOutputs.get()}));
         addParameter(keep.set("Keep", false));
         addParameter(event.set("Event", false));
 
-        addOutputParameter(output1.set("Output 1", {0}, {-FLT_MAX}, {FLT_MAX}));
-        addOutputParameter(output2.set("Output 2", {0}, {-FLT_MAX}, {FLT_MAX}));
+        int n = numOutputs.get();
+        outputs.resize(n);
+        lastOutputs.resize(n);
+        for (int i = 0; i < n; i++) {
+            outputs[i].set("Output " + ofToString(i + 1), {0.0f}, {-FLT_MAX}, {FLT_MAX});
+            addOutputParameter(outputs[i]);
+        }
 
-        prev_routeTo = routeTo.get();
+        listeners.push(numOutputs.newListener([this](int &) {
+            updateNumOutputs();
+        }));
 
-        // Listener for input changes
-        inputListener = input.newListener([this](vector<float> &in) {
-            if (!event.get() || (event.get() && in != lastInputValue)) {
+        listeners.push(input.newListener([this](vector<float> &in) {
+            if (!event.get() || in != lastInputValue) {
                 updateOutputs();
-                lastInputValue = in; // Store the last input value
+                lastInputValue = in;
             }
-        });
+        }));
 
-        routeListener = routeTo.newListener([this](vector<int> &route) {
-            if(routeUpdateNeeded || !event.get()) {
-                updateOutputs();
-            }
-            routeUpdateNeeded = false; // Reset after each update
-        });
-
-        keepListener = keep.newListener([this](bool &k) {
+        listeners.push(routeTo.newListener([this](vector<int> &) {
             updateOutputs();
-        });
+        }));
+
+        listeners.push(keep.newListener([this](bool &) {
+            updateOutputs();
+        }));
     }
 
 private:
+    void updateNumOutputs() {
+        int oldSize = (int)outputs.size();
+        int newSize = numOutputs.get();
+
+        if (newSize < oldSize) {
+            for (int i = oldSize - 1; i >= newSize; i--) {
+                removeParameter("Output " + ofToString(i + 1));
+            }
+            outputs.resize(newSize);
+            lastOutputs.resize(newSize);
+        } else if (newSize > oldSize) {
+            outputs.resize(newSize);
+            lastOutputs.resize(newSize);
+            for (int i = oldSize; i < newSize; i++) {
+                outputs[i].set("Output " + ofToString(i + 1), {0.0f}, {-FLT_MAX}, {FLT_MAX});
+                addOutputParameter(outputs[i]);
+            }
+        }
+
+        routeTo.setMax(vector<int>(1, newSize));
+        updateOutputs();
+    }
+
     void updateOutputs() {
-        vector<float> newOutput1(input.get().size(), 0);  // Initialize with zeros
-        vector<float> newOutput2(input.get().size(), 0);  // Initialize with zeros
+        int n = (int)outputs.size();
+        int inSize = (int)input.get().size();
 
-        int defaultRoute = routeTo.get().size() == 1 ? routeTo.get()[0] : -1;  // Check if routeTo is scalar
+        vector<vector<float>> newOutputs(n, vector<float>(inSize, 0.0f));
 
-        for (size_t i = 0; i < input.get().size(); i++) {
-            int route = defaultRoute != -1 ? defaultRoute : (i < routeTo.get().size() ? routeTo.get()[i] : 0); // Use defaultRoute if scalar, else use routeTo vector
-            if (route == 1) {
-                newOutput1[i] = input.get()[i];
-                if (keep.get()) {
-                    newOutput2[i] = lastOutput2.size() > i ? lastOutput2[i] : 0;  // Keep last value or use zero if not available
-                }
-            } else {
-                newOutput2[i] = input.get()[i];
-                if (keep.get()) {
-                    newOutput1[i] = lastOutput1.size() > i ? lastOutput1[i] : 0;  // Keep last value or use zero if not available
+        bool isScalar = (routeTo.get().size() == 1);
+        int defaultRoute = isScalar ? routeTo.get()[0] : -1;
+
+        for (int i = 0; i < inSize; i++) {
+            int route = isScalar ? defaultRoute
+                                 : (i < (int)routeTo.get().size() ? routeTo.get()[i] : 1);
+            route = ofClamp(route, 1, n);
+            int routeIdx = route - 1;
+
+            newOutputs[routeIdx][i] = input.get()[i];
+
+            if (keep.get()) {
+                for (int j = 0; j < n; j++) {
+                    if (j != routeIdx) {
+                        newOutputs[j][i] = ((int)lastOutputs[j].size() > i) ? lastOutputs[j][i] : 0.0f;
+                    }
                 }
             }
         }
 
-        output1.set(newOutput1);
-        output2.set(newOutput2);
-        lastOutput1 = newOutput1;  // Store the last outputs
-        lastOutput2 = newOutput2;
+        for (int j = 0; j < n; j++) {
+            outputs[j].set(newOutputs[j]);
+            lastOutputs[j] = newOutputs[j];
+        }
     }
 
-    // Member variables to store the last outputs
-    vector<float> lastOutput1;
-    vector<float> lastOutput2;
+    ofParameter<int> numOutputs;
     ofParameter<vector<float>> input;
-    ofParameter<vector<int>> routeTo; // Changed to vector of ints
+    ofParameter<vector<int>> routeTo;
     ofParameter<bool> keep;
-    ofParameter<bool> event; // The event toggle
+    ofParameter<bool> event;
 
-    ofParameter<vector<float>> output1;
-    ofParameter<vector<float>> output2;
+    vector<ofParameter<vector<float>>> outputs;
+    vector<vector<float>> lastOutputs;
+    vector<float> lastInputValue;
 
-    vector<int> prev_routeTo; // Updated to vector<int>
-    vector<float> lastInputValue; // To keep track of the last received input
-
-    ofEventListener inputListener;
-    ofEventListener routeListener;
-    ofEventListener keepListener;
-
-    bool routeUpdateNeeded;
+    ofEventListeners listeners;
 };
 
 #endif /* distribute_h */
